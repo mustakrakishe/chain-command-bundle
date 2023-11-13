c1f2c7f67a2afa8f0f9a51c28e5e8e7610d9d9c0
mustakrakishe <mustakrkish@gmail.com>
2023-11-13 19:59:33 +0200

    core: install chain command bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Mustakrakishe\FooBundle\MustakrakisheFooBundle::class => ['all' => true],
     Mustakrakishe\BarBundle\MustakrakisheBarBundle::class => ['all' => true],
+    Mustakrakishe\ChainCommandBundle\MustakrakisheChainCommandBundle::class => ['all' => true],
 ];
