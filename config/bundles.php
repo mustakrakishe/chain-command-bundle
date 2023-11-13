2a7d8970c6e48782aef8a3c46affd09f5d62736c
mustakrakishe <mustakrkish@gmail.com>
2023-11-13 19:45:32 +0200

    core: install bar bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Mustakrakishe\FooBundle\MustakrakisheFooBundle::class => ['all' => true],
+    Mustakrakishe\BarBundle\MustakrakisheBarBundle::class => ['all' => true],
 ];
